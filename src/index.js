8a2d02fc2deb1af6e3b54294f51fcb4dc05e471a
JeffHallCarProof <JHall@carproof.com>
2018-10-23 14:39:56 -0400

    Greensock Base animation
    2 logos slide across each other.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+import SimpleTween from "./animation.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
 
+  render() {
+    return (
+      <div className="container">
+        <SimpleTween />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
